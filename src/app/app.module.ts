bc2ddf6eeb34100c86db8e68b01402df1fdc0bdd
Josh0Sprime <jorgepro121313@hotmail.com>
2023-01-31 01:32:53 -0300

    Primer commit de navbar y pagina home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
